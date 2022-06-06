be1ed7d580dcb5000021ca730cf6f74e570ecef3
Dileep C Unnikrishnan <39225025+drdileepunni@users.noreply.github.com>
2022-06-06 11:22:22 +0530

    Added code to send json files to the api

--- lambda_function.py
+++ lambda_function.py
@@ -4,14 +4,10 @@ def lambda_handler(event, context):
 
     # Gathering data from event
     data = event.get('data')
-    print(data)
 
-    report_json = {
-	"Hi there": "Hello"
-	}
     return {
         'statusCode': 200,
-        'body': report_json
+        'body': data
     }
 
 #f=open("lambda_trial.json",)
--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-INPUTDATA='{"My trial string":"trial"}'
+INPUTDATA=`cat lambda_trial.json`
 curl -XPOST "http://localhost:9000/2015-03-31/functions/function/invocations" -d "$INPUTDATA"
